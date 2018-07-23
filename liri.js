f089ad50a8cb468abb1ec45eb3a0a15067bca8a6
mpeng <michaelxpeng@gmail.com>
2018-07-23 11:17:43 -0400

    basic files

---
+++ liri.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+var keys = require('./keys.js');
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
+
+// my-tweets
+
+// spotify-this-song
+
+// movie-this
+
+// do-what-it-says
